125f2f194521d7c6d96baa2adc8e8474d8bd02f6
Dinh <119478119+dinh292@users.noreply.github.com>
2023-03-13 01:33:25 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,44 @@ import './App.css'
 import Avatar from './components/Avatar'
 import PartList from './components/PartList'
 const totalParts = {
-  body: 17,
-  eyes: 17,
-  hair: 73,
-  hat: 28,
-  mouth: 24,
-  eyebrows: 15,
-  glasses: 17,
+  body: 5,
+  eyes: 5,
+  hair: 10,
+  mouth: 5,
+  eyebrows: 5,
+  glasses: 5,
   clothing1: 5,
   clothing2: 5,
   clothing3: 9
 }
 const App = () => {
-  // Defining state variables to keep track of the current selection for each part of the avatar
-  const [body, setBody] = useState(0)
-  const [eyes, setEyes] = useState(0)
-  const [eyebrows, setEyebrow] = useState(0)
-  const [hair, setHair] = useState(0)
-  const [hat, setHat] = useState(0)
-  const [clothing1, setClothing1] = useState(0)
-  const [clothing2, setClothing2] = useState(0)
-  const [clothing3, setClothing3] = useState(0)
-  const [mouth, setMouth] = useState(0)
-  const [glasses, setGlasses] = useState(0)
-  // Defining the randomize function which will select a random value for each part
+  const [state, setState] = useState({
+    body: 0,
+    eyes: 0,
+    eyebrows: 0,
+    hair: 0,
+    mouth: 0,
+    glasses: 0,
+    clothing1: 0,
+    clothing2: 0,
+    clothing3: 0
+  })
   const randomize = () => {
     const getRandomValue = (part) => {
       return Math.floor(Math.random() * totalParts[part])
     }
-    setBody(getRandomValue('body'))
-    setEyes(getRandomValue('eyes'))
-    setEyebrow(getRandomValue('eyebrows'))
-    setHair(getRandomValue('hair'))
-    setHat(getRandomValue('hat'))
-    setMouth(getRandomValue('mouth'))
-    setGlasses(getRandomValue('glasses'))
-    setClothing1(getRandomValue('clothing1'))
-    setClothing2(getRandomValue('clothing2'))
-    setClothing3(getRandomValue('clothing3'))
+    setState({
+      body: getRandomValue('body'),
+      eyes: getRandomValue('eyes'),
+      eyebrows: getRandomValue('eyebrows'),
+      hair: getRandomValue('hair'),
+      mouth: getRandomValue('mouth'),
+      glasses: getRandomValue('glasses'),
+      clothing1: getRandomValue('clothing1'),
+      clothing2: getRandomValue('clothing2'),
+      clothing3: getRandomValue('clothing3')
+    })
   }
-  // Using the useEffect hook to automatically randomize the avatar on page load
   useEffect(() => {
     randomize()
   }, [])
@@ -51,16 +49,15 @@ const App = () => {
       <div className='col-4'>
         <div className='avatar-wrapper'>
           <Avatar
-            body={body}
-            eyes={eyes}
-            hair={hair}
-            hat={hat}
-            clothing1={clothing1}
-            clothing2={clothing2}
-            clothing3={clothing3}
-            mouth={mouth}
-            eyebrow={eyebrows}
-            glasses={glasses}
+            body={state.body}
+            eyes={state.eyes}
+            hair={state.hair}
+            clothing1={state.clothing1}
+            clothing2={state.clothing2}
+            clothing3={state.clothing3}
+            mouth={state.mouth}
+            eyebrow={state.eyebrows}
+            glasses={state.glasses}
           />
         </div>
         <button className='button' onClick={() => randomize()}>
@@ -69,44 +66,40 @@ const App = () => {
       </div>
       <div className='col-8'>
         <details className='list-section'>
-          <summary>Hat</summary>
-          <PartList total={totalParts.hat} path='accessories/hats' set={setHat} selected={hat} />
-        </details>
-        <details className='list-section'>
           <summary>Hair</summary>
-          <PartList total={totalParts.hair} path='hair' set={setHair} selected={hair} />
+          <PartList total={totalParts.hair} path='hair' set={(value) => setState({ ...state, hair: value })} selected={state.hair} />
         </details>
         <details className='list-section'>
           <summary>Body</summary>
-          <PartList total={totalParts.body} path='body' set={setBody} selected={body} />
+          <PartList total={totalParts.body} path='body' set={(value) => setState({ ...state, body: value })} selected={state.body} />
         </details>
         <details className='list-section'>
           <summary>Eyebrows</summary>
-          <PartList total={totalParts.eyebrows} path='eyebrows' set={setEyebrow} selected={eyebrows} />
+          <PartList total={totalParts.eyebrows} path='eyebrows' set={(value) => setState({ ...state, eyebrows: value })} selected={state.eyebrows} />
         </details>
         <details className='list-section'>
           <summary>Eyes</summary>
-          <PartList total={totalParts.eyes} path='eyes' set={setEyes} selected={eyes} top='35px' />
+          <PartList total={totalParts.eyes} path='eyes' set={(value) => setState({ ...state, eyes: value })} selected={state.eyes} top='35px' />
         </details>
         <details className='list-section'>
           <summary>Mouth</summary>
-          <PartList total={totalParts.mouth} path='mouths' set={setMouth} selected={mouth} />
+          <PartList total={totalParts.mouth} path='mouth' set={(value) => setState({ ...state, mouth: value })} selected={state.mouth} />
         </details>
         <details className='list-section'>
           <summary>Glasses</summary>
-          <PartList total={totalParts.glasses} path='accessories/glasses' set={setGlasses} selected={glasses} />
+          <PartList total={totalParts.glasses} path='accessories/glasses' set={(value) => setState({ ...state, glasses: value })} selected={state.glasses} />
         </details>
         <details className='list-section'>
           <summary>Clothing (L1)</summary>
-          <PartList total={totalParts.clothing1} path='clothes/layer_1' set={setClothing1} selected={clothing1} />
+          <PartList total={totalParts.clothing1} path='clothes/layer_1' set={(value) => setState({ ...state, clothing1: value })} selected={state.clothing1} />
         </details>
         <details className='list-section'>
           <summary>Clothing (L2)</summary>
-          <PartList total={totalParts.clothing2} path='clothes/layer_2' set={setClothing2} selected={clothing2} />
+          <PartList total={totalParts.clothing2} path='clothes/layer_2' set={(value) => setState({ ...state, clothing2: value })} selected={state.clothing2} />
         </details>
         <details className='list-section'>
           <summary>Clothing (L3)</summary>
-          <PartList total={totalParts.clothing3} path='clothes/layer_3' set={setClothing3} selected={clothing3} />
+          <PartList total={totalParts.clothing3} path='clothes/layer_3' set={(value) => setState({ ...state, clothing3: value })} selected={state.clothing3} />
         </details>
       </div>
     </div>
